83c5a0e71191fae8934cf61b44b566a218b6231f
riko <riyad.dashdamirli@gmail.com>
2026-03-23 16:29:55 +0000

    fixed the turtlebot not being rendered

--- launch/mars_launch.py
+++ launch/mars_launch.py
@@ -5,15 +5,16 @@ from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    urdf_file = os.path.join(
-        get_package_share_directory('turtlebot3_description'),
-        'urdf', 'turtlebot3_burger.urdf'
-    )
+    tb3_share = get_package_share_directory('turtlebot3_description')
+    urdf_file = os.path.join(tb3_share, 'urdf', 'turtlebot3_burger.urdf')
     with open(urdf_file, 'r') as f:
         robot_desc = f.read()
     env = os.environ.copy()
     env['LIBGL_ALWAYS_SOFTWARE'] = '1'
     env['IGN_LOG_PATH'] = '/tmp/ign_logs'
+    env['IGN_GAZEBO_RESOURCE_PATH'] = os.path.dirname(tb3_share) + (
+        ':' + env['IGN_GAZEBO_RESOURCE_PATH'] if 'IGN_GAZEBO_RESOURCE_PATH' in env else ''
+    )
     return LaunchDescription([
         # Gazebo starten
         ExecuteProcess(
